64dd98f75aa6f65e58d6ca6a3918a98ccef3fa8f
Giacomo Ciotola <cassetto.attrezzi@gmail.com>
2018-01-12 23:44:41 +0100

    20180112: update

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,27 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Array containing list of english numbers
+        ArrayList<String> englishNumbers= new ArrayList<String>();
+        englishNumbers.add("one");
+        englishNumbers.add("two");
+        englishNumbers.add("three");
+        englishNumbers.add("four");
+        englishNumbers.add("five");
+        englishNumbers.add("six");
+        englishNumbers.add("seven");
+        englishNumbers.add("eight");
+        englishNumbers.add("nine");
+        englishNumbers.add("ten");
+
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+
+        for(int i = 0;i<englishNumbers.size();i++) {
+            TextView textView = new TextView(this);
+            textView.setText(englishNumbers.get(i));
+            rootView.addView(textView);
+            Log.v("NumbersActivity", "Number at index " + i + " = " + englishNumbers.get(i));
+        }
     }
 }
