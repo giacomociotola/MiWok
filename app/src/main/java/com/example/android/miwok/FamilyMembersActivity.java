d25aaa71d08b45fafc3c1db0ad3a4184cfb8124f
Giacomo Ciotola <cassetto.attrezzi@gmail.com>
2018-01-20 13:52:21 +0100

    20180120: update - added audio files

--- app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyMembersActivity.java
@@ -1,13 +1,49 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyMembersActivity extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family_members);
+        setContentView(R.layout.word_list);
+
+        // Array containing list of english words members
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word(R.drawable.family_father,"father", "әpә", R.raw.family_father));
+        words.add(new Word(R.drawable.family_mother,"mother", "әṭa", R.raw.family_mother));
+        words.add(new Word(R.drawable.family_son,"son", "angsi", R.raw.family_son));
+        words.add(new Word(R.drawable.family_daughter,"daughter", "tune", R.raw.family_daughter));
+        words.add(new Word(R.drawable.family_older_brother,"older brother", "taachi", R.raw.family_older_brother));
+        words.add(new Word(R.drawable.family_younger_brother,"younger brother", "chalitti", R.raw.family_younger_brother));
+        words.add(new Word(R.drawable.family_older_sister,"older sister", "teṭe", R.raw.family_older_sister));
+        words.add(new Word(R.drawable.family_younger_sister,"younger sister", "kolliti", R.raw.family_younger_sister));
+        words.add(new Word(R.drawable.family_grandmother,"grandmother", "ama", R.raw.family_grandmother));
+        words.add(new Word(R.drawable.family_grandfather,"grandfather", "paapa", R.raw.family_grandfather));
+
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_family);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                Word currentWord = words.get(position);
+                Log.v("FamilyMembersActivity", "Current word: " + currentWord);
+                mMediaPlayer = MediaPlayer.create(FamilyMembersActivity.this, currentWord.getmAudioResourceId());
+                mMediaPlayer.start();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,40 +1,48 @@
 package com.example.android.miwok;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
+    private MediaPlayer mMediaPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
 
-        // Array containing list of english numbers
-        ArrayList<String> englishNumbers= new ArrayList<String>();
-        englishNumbers.add("one");
-        englishNumbers.add("two");
-        englishNumbers.add("three");
-        englishNumbers.add("four");
-        englishNumbers.add("five");
-        englishNumbers.add("six");
-        englishNumbers.add("seven");
-        englishNumbers.add("eight");
-        englishNumbers.add("nine");
-        englishNumbers.add("ten");
+        // Array containing list of english words
+        final ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word(R.drawable.number_one,"one", "lutti", R.raw.number_one));
+        words.add(new Word(R.drawable.number_two,"two", "otiiko", R.raw.number_two));
+        words.add(new Word(R.drawable.number_three,"three", "tolookosu", R.raw.number_three));
+        words.add(new Word(R.drawable.number_four,"four", "oyyisa", R.raw.number_four));
+        words.add(new Word(R.drawable.number_five,"five", "massokka", R.raw.number_five));
+        words.add(new Word(R.drawable.number_six,"six", "temmokka", R.raw.number_six));
+        words.add(new Word(R.drawable.number_seven,"seven", "kenekaku", R.raw.number_seven));
+        words.add(new Word(R.drawable.number_eight,"eight", "kawinta", R.raw.number_eight));
+        words.add(new Word(R.drawable.number_nine,"nine", "wo'e", R.raw.number_nine));
+        words.add(new Word(R.drawable.number_ten,"ten", "na'aacha", R.raw.number_ten));
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-
-        for(int i = 0;i<englishNumbers.size();i++) {
-            TextView textView = new TextView(this);
-            textView.setText(englishNumbers.get(i));
-            rootView.addView(textView);
-            Log.v("NumbersActivity", "Number at index " + i + " = " + englishNumbers.get(i));
-        }
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                Word currentWord = words.get(position);
+                Log.v("NumbersActivity", "Current word: " + currentWord);
+                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, currentWord.getmAudioResourceId());
+                mMediaPlayer.start();
+            }
+        });
     }
 }
